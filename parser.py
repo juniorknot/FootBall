ae5cf69127f315e5f1787834aec1558d0277e046
makcmoscow <makcmoscow@gmail.com>
2019-02-11 21:31:26 +0300

    well done

--- parser.py
+++ parser.py
@@ -38,51 +38,58 @@ class Parser:
             list_of_games = page.find_elements_by_class_name('c-events__item_game')
             print('Всего игр на странице: ', len(list_of_games))
             self.driver.minimize_window()
-            for game in list_of_games:
-                balls_ok = False
-                link_to_game = game.find_element_by_class_name('c-events-scoreboard').find_element_by_tag_name('a').get_attribute('href')
-                try:
-                    game_time = int(game.find_element_by_class_name('c-events__time').text.split(':')[0])
-                except ValueError:
-                    game_time = gtime + 5
-                except NoElement:
-                    game_time = gtime + 5
-                game_ID = link_to_game.split('/')[-2].split('-')[0]
-                print(game_ID)
-                balls_list = game.find_elements_by_class_name('c-events-scoreboard__cell--all')
-                if len(balls_list)>1:
-                    # if int(balls_list[0].text)>1 or int(balls_list[1].text)>1:
-                    if (int(balls_list[0].text) + int(balls_list[1].text)) > 1:
-                        balls_ok = True
-
-                if balls_ok and game_time <gtime and game_ID not in matches:
-                # if 45>game_time>0:
-                    game_page = GamePage(link_to_game)
-
-                    print('Now we\'ll make the bet')
+            if len(list_of_games)>0:
+                for game in list_of_games:
+                    balls_ok = False
                     try:
-                        game_page.login()
+                        link_to_game = game.find_element_by_class_name('c-events-scoreboard').find_element_by_tag_name('a').get_attribute('href')
                     except Exception:
-                        pass
-
+                        time.sleep(300)
+                        break
                     try:
-                        game_page.make_bet()
-                        matches.append(game_ID)
-                    except Exception as e:
-                        errors.append(e)
-                        print(errors)
-                    game_page.driver.quit()
+                        game_time = int(game.find_element_by_class_name('c-events__time').text.split(':')[0])
+                    except ValueError:
+                        game_time = gtime + 5
+                    except NoElement:
+                        game_time = gtime + 5
+                    game_ID = link_to_game.split('/')[-2].split('-')[0]
+                    print(game_ID)
+                    balls_list = game.find_elements_by_class_name('c-events-scoreboard__cell--all')
+                    if len(balls_list)>1:
+                        # if int(balls_list[0].text)>1 or int(balls_list[1].text)>1:
+                        if (int(balls_list[0].text) + int(balls_list[1].text)) > 1:
+                            balls_ok = True
 
-                if game_time >=gtime:
-                    if game_ID in matches:
-                        matches.remove(game_ID)
+                    if balls_ok and game_time <gtime and game_ID not in matches:
+                    # if 45>game_time>0:
+                        game_page = GamePage(link_to_game)
 
-                print('Time: ', game_time, balls_list[0], ':', balls_list[1])
-                print(link_to_game)
-                print('matches: ', matches)
-            # self.driver.quit()
+                        print('Now we\'ll make the bet')
+                        try:
+                            game_page.login()
+                        except Exception:
+                            pass
+
+                        try:
+                            game_page.make_bet()
+                            matches.append(game_ID)
+                        except Exception as e:
+                            errors.append(e)
+                            print(errors)
+                        game_page.driver.quit()
+
+                    if game_time >=gtime:
+                        if game_ID in matches:
+                            matches.remove(game_ID)
+
+                    print('Time: ', game_time)#, balls_list[0], ':', balls_list[1])
+                    print(link_to_game)
+                    print('matches: ', matches)
+                # self.driver.quit()
         except NotAttached:
             # self.driver.quit()
+            pass
+        except Exception:
             pass
 
     def going_by_mirror(self):
@@ -211,7 +218,7 @@ class GamePage:
 
     def make_bet(self):
         # self.login()
-        time.sleep(1)
+        time.sleep(2)
         box_to_choose = self.driver.find_element_by_class_name('dopEvsWrap')
         box_to_choose.click()
         time.sleep(1)
@@ -219,11 +226,12 @@ class GamePage:
         time1.click()
         bets = self.driver.find_elements_by_xpath('//span[@class = "bet_type"]')
         for bet in bets:
-            if bet.text == '2.5 Б':
+        	# if bet.text == '2.5 Б':
+            if bet.text == '3 Б':
                 bet.click()
                 time.sleep(0.5)
                 input_window = self.driver.find_element_by_xpath('//input[@class = "c-spinner__input bet_sum_input"]')
-                input_window.send_keys('20')
+                input_window.send_keys('50')
                 time.sleep(0.5)
                 input_window.send_keys(Keys.ENTER)
                 time.sleep(15)
